964f28029ce22e27c49219f2347e860159d14f04
Pablo Tato Ramos <pabtatoramos@gmail.com>
2019-02-28 19:45:02 +0100

    puta mierda

--- q_round.py
+++ q_round.py
@@ -11,12 +11,43 @@ for i in range(n_photos):
     words = input().split()
     photos.append(Photo(i, words[0], words[2:]))
 
+if 0:
+    tags = set()
+    for p in photos:
+        tags = tags.union(set(p.tags))
+    tags_ids = dict()
+    for i,t in enumerate(tags):
+        tags_ids[t] = i
+    for p in photos:
+        tags_as_ids = [ tags_ids[t] for t in p.tags ]
+        p.tags = tags_as_ids
+
 
 horizontals = [ p for p in photos if p.orient == 'H' ]
 verticals = [ p for p in photos if p.orient == 'V' ]
 
-print(len(horizontals) + len(verticals)//2)
-for h in horizontals:
-    print(h.id)
+slides = [ horizontals[0] ]
+del horizontals[0]
+
+def compare(xs, ys):
+    common = 0
+    for x in xs.tags:
+        if x in ys.tags:
+            common += 1
+    return min(len(xs.tags)-common, len(ys.tags)-common, common)
+
+while len(horizontals) > 0:
+    previous = slides[len(slides)-1]
+    best = sorted(horizontals, key=lambda p: compare(p, previous))[0]
+    slides.append(best)
+    horizontals.remove(best)
+    print("{} slides, {} photos".format(len(slides), len(photos)))
+
+print(len(slides) + len(verticals)//2)
+
+for s in slides:
+    print(s.id)
+
 for i in range(0, len(verticals)//2*2, 2):
     print(str(verticals[i].id) + ' ' + str(verticals[i+1].id))
+
